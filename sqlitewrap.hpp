a805fa3a7d45cb1bf2ad1ac50cffebae22465322
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-10-25 13:42:40 +0200

    add namespace

--- sqlitewrap.hpp
+++ sqlitewrap.hpp
@@ -1,8 +1,5 @@
 /*
-SQLITE C++ DB GENERATOR
-Command line tool to create a sqlite db from:
-- csv
-- json   (coming soon)
+SQLITE C++ DB WRAPPER
 */
 
 #include <iostream>
@@ -12,11 +9,10 @@ Command line tool to create a sqlite db from:
 #include <boost/algorithm/string.hpp>
 #include <sqlite3.h>
 
-using namespace std;
-
 #define NULL_CALLBACK    NULL
 
-enum{
+enum
+{
   SQLERR_DBERR   = 1,
   SQLERR_GENERIC = 2
 };
@@ -24,9 +20,9 @@ enum{
 class table_schema
 {
 public:
-  vector<string> headers, types;
+  std::vector<std::string> headers, types;
 
-  void insert(string h, string t = "TEXT NOT NULL")
+  void insert(std::string h, std::string t = "TEXT NOT NULL")
   {
     headers.push_back(h);
     types.push_back(t);
@@ -41,11 +37,11 @@ public:
 
   table_schema table;
 
-  sqlitedb(const string &db_name)
+  sqlitedb(const std::string &db_name)
   {
     if( sqlite3_open(db_name.c_str(), &db) != SQLITE_OK )
     {
-      cerr << "Can't open database: " << sqlite3_errmsg(db) << endl;
+      std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
       sqlite3_close(db);
       exit(SQLERR_DBERR);
     }
@@ -56,75 +52,75 @@ public:
     sqlite3_close(db);
   }
 
-  void add_table(const string &table_name, vector<string> &header)
+  void add_table(const std::string &table_name, std::vector<std::string> &header)
   {
     sqlite3_free(err_msg);
 
-    string cmd;
+    std::string cmd;
     cmd = "CREATE TABLE " + table_name + "(\n";
-    for(size_t i=0; i<header.size(); ++i) cmd += header[i] + " TEXT KEY NOT NULL" + ((i==header.size()-1)?"":",") + "\n";
+    for(int i=0; i<(int)header.size(); ++i) cmd += header[i] + " TEXT KEY NOT NULL" + ((i==header.size()-1)?"":",") + "\n";
     cmd += ");";
 
     if( sqlite3_exec(db, cmd.c_str(), NULL_CALLBACK, 0, &err_msg) != SQLITE_OK )
     {
-      cerr << "SQL_ERROR: " << err_msg << endl;
+      std::cerr << "SQL_ERROR: " << err_msg << std::endl;
     }
   }
 
-  void add_table(const string &table_name, vector<string> &header, vector<string> &types)
+  void add_table(const std::string &table_name, std::vector<std::string> &header, std::vector<std::string> &types)
   {
     sqlite3_free(err_msg);
 
-    string cmd;
+    std::string cmd;
     cmd = "CREATE TABLE " + table_name + "(\n";
-    for (size_t i = 0; i<header.size(); ++i) cmd += header[i] + " " + types[i] + ((i == header.size() - 1) ? "" : ",") + "\n";
+    for (int i = 0; i<(int)header.size(); ++i) cmd += header[i] + " " + types[i] + ((i == header.size() - 1) ? "" : ",") + "\n";
     cmd += ");";
 
     if (sqlite3_exec(db, cmd.c_str(), NULL_CALLBACK, 0, &err_msg) != SQLITE_OK)
     {
-      cerr << "SQL_ERROR: " << err_msg << endl;
+      std::cerr << "SQL_ERROR: " << err_msg << std::endl;
     }
   }
 
-  void add_table(const string &table_name)
+  void add_table(const std::string &table_name)
   {
     add_table(table_name, table.headers, table.types);
   }
 
-  void add_entry(const string &table_name, vector<string> values)
+  void add_entry(const std::string &table_name, std::vector<std::string> values)
   {
     sqlite3_free(err_msg);
 
-    string cmd;
+    std::string cmd;
     cmd = "INSERT INTO " + table_name + " VALUES (";
-    for(size_t i=0; i<values.size(); ++i) cmd += "'" + values[i] + "'" + ((i==values.size()-1)?"":", ");
+    for(int i=0; i<(int)values.size(); ++i) cmd += "'" + values[i] + "'" + ((i==values.size()-1)?"":", ");
     cmd += ");";
 
     if( sqlite3_exec(db, cmd.c_str(), NULL_CALLBACK, NULL, &err_msg) != SQLITE_OK )
     {
-      cerr << "SQL_ERROR: " << err_msg << endl;
+      std::cerr << "SQL_ERROR: " << err_msg << std::endl;
     }
   }
 
-  template<typename T> void query(const string &table_name, int (*cb)(void *, int, char **, char **), T* ptr = nullptr)
+  template<typename T> void query(const std::string &table_name, int (*cb)(void *, int, char **, char **), T* ptr = nullptr)
   {
     sqlite3_free(err_msg);
 
-    string cmd = "SELECT * FROM " + string(table_name) + ";";
+    std::string cmd = "SELECT * FROM " + std::string(table_name) + ";";
 
     if( sqlite3_exec(db, cmd.c_str(), cb, (void *) ptr, &err_msg) != SQLITE_OK )
     {
-      cerr << "SQL_ERROR: " << err_msg << endl;
+      std::cerr << "SQL_ERROR: " << err_msg << std::endl;
     }
   }
 
-  template<typename T> void custom_query(const string &table_name, const string &cmd, int(*cb)(void *, int, char **, char **), T* ptr = nullptr)
+  template<typename T> void custom_query(const std::string &table_name, const std::string &cmd, int(*cb)(void *, int, char **, char **), T* ptr = nullptr)
   {
     sqlite3_free(err_msg);
 
     if (sqlite3_exec(db, cmd.c_str(), cb, (void *)ptr, &err_msg) != SQLITE_OK)
     {
-      cerr << "SQL_ERROR: " << err_msg << endl;
+      std::cerr << "SQL_ERROR: " << err_msg << std::endl;
     }
   }
 
@@ -140,11 +136,11 @@ public:
   {
     sqlite3_free(err_msg);
 
-    string cmd = "BEGIN TRANSACTION";
+    std::string cmd = "BEGIN TRANSACTION";
 
     if (sqlite3_exec(db, cmd.c_str(), NULL_CALLBACK, NULL, &err_msg) != SQLITE_OK)
     {
-      cerr << "SQL_ERROR: " << err_msg << endl;
+      std::cerr << "SQL_ERROR: " << err_msg << std::endl;
     }
   }
 
@@ -152,11 +148,11 @@ public:
   {
     sqlite3_free(err_msg);
 
-    string cmd = "COMMIT TRANSACTION";
+    std::string cmd = "COMMIT TRANSACTION";
 
     if (sqlite3_exec(db, cmd.c_str(), NULL_CALLBACK, NULL, &err_msg) != SQLITE_OK)
     {
-      cerr << "SQL_ERROR: " << err_msg << endl;
+      std::cerr << "SQL_ERROR: " << err_msg << std::endl;
     }
   }
 };
